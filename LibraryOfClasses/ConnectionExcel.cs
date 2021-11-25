7fd3eeff3f17266a96772ef6931906ab4240f4d3
SerjPi <monsup@mail.ru>
2021-11-25 16:10:00 +0300

    Первый релиз

---
+++ LibraryOfClasses/ConnectionExcel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LibraryOfClasses
+{
+    /// <summary>
+    /// Класс взаимодействуе с файлом Excel
+    /// вытягивает из него данные в обьект DataSet
+    /// </summary>
+    public class ExtractExcelFiletoDateSet
+    {
+        public string ConnectionString { get; private set; }
+        public DataSet ExcelFileDataSet { get; private set; }
+
+
+        public ExtractExcelFiletoDateSet( string connectionString, string fileName )
+        {
+            ConnectionString = connectionString;
+            ExcelFileDataSet = new DataSet( fileName );
+            Parse( fileName );
+        }
+
+        private  void Parse( string fileName )
+        {
+            //string connectionString = string.Format( "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=YES;IMEX=1;';" );
+            OleDbConnectionStringBuilder build = new OleDbConnectionStringBuilder( ConnectionString );
+            build[ "Data Source" ] = fileName;
+
+            //DataSet excelDataSet = new DataSet( );
+
+            foreach (var sheetName in GetExcelSheetNames( build.ConnectionString ))
+            {
+                using (OleDbConnection con = new OleDbConnection( build.ConnectionString ))
+                {
+                    var listExcelDT = new DataTable( sheetName );
+                    string query = $"SELECT * FROM [{sheetName}]";
+                    con.Open( );
+                    OleDbDataAdapter adapter = new OleDbDataAdapter( query, con );
+                    adapter.Fill( listExcelDT );
+                    ExcelFileDataSet.Tables.Add( listExcelDT );
+                }
+            }
+
+            //return excelDataSet;
+        }
+
+        /// <summary>
+        /// Получить список листов в файле EXcel        /// 
+        /// </summary>
+        /// <param name="_connectionString"></param>
+        /// <returns></returns>
+        private List<string> GetExcelSheetNames( string _connectionString )
+        {
+            OleDbConnection con = null;
+            DataTable dt = null;
+            con = new OleDbConnection( _connectionString );
+            con.Open( );
+            dt = con.GetOleDbSchemaTable( OleDbSchemaGuid.Tables, null );
+
+            if (dt == null)
+            {
+                return null;
+            }
+
+            List<string> excelSheetNames = new List<string>();
+            //int i = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (!row[ "TABLE_NAME" ].ToString( ).Contains( "Print_Area" ))
+                {
+                    excelSheetNames.Add( row[ "TABLE_NAME" ].ToString( ) );
+                }
+                //excelSheetNames[ i ] = row[ "TABLE_NAME" ].ToString( );
+                //i++;
+            }
+
+            return excelSheetNames;
+        }
+
+
+        /// <summary>
+        /// Поиск слова в DataSet
+        /// </summary>
+        /// <param name="wordFound"></param>
+        public void SearchWordInDataSet( string wordFound )
+        {
+            foreach (DataTable curDt in ExcelFileDataSet.Tables)
+            {
+                foreach (DataRow dataRow in curDt.Rows)
+                {
+                    foreach (var item in dataRow.ItemArray)
+                    {
+                        if (item.ToString().Trim().ToLowerInvariant().Equals(wordFound.Trim().ToLowerInvariant()))
+                        {
+                            PrintDataRow( dataRow, curDt, ExcelFileDataSet );
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PrintDataRow( DataRow dataRow, DataTable curDt, DataSet excelFileDataSet )
+        {
+            Console.Write( $"[ {Path.GetFileName( excelFileDataSet.DataSetName)} ] [ {curDt.TableName} ] --->" );
+            foreach (var item in dataRow.ItemArray)
+            {
+                Console.Write( $"{item.ToString( )} " );
+            }
+            Console.WriteLine( );
+        }
+
+        //private void PrintDataRow( DataRow dataRow )
+        //{
+        //    Console.WriteLine( "------" );
+        //    foreach (var item in dataRow.ItemArray)
+        //    {
+        //        Console.Write( $"{item.ToString( )} " );
+        //    }
+            
+        //}
+    }
+}
--- SearchInExcel/Program.cs
+++ SearchInExcel/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryOfClasses;
+using System.Configuration;
+using System.IO;
+using System.Data;
 
 namespace SearchInExcel
 {
@@ -10,6 +14,48 @@ namespace SearchInExcel
     {
         static void Main( string[] args )
         {
+            string wordForSearch = null;
+            string stopWord = "й";
+            bool isWork = true;
+            while (isWork)
+            {
+                Console.WriteLine( new string( '-', 100 ) );
+                Console.WriteLine( "Введите децимальный номер для поиска:" );
+                wordForSearch = Console.ReadLine();
+                List<string> resultSearch = SearchWords( wordForSearch );
+                Console.WriteLine($"Нажмите \"{stopWord}\" чтобы завершить работу или любую клавишу чтобы продолжить...");
+                if (Console.ReadLine( ).ToLowerInvariant() == stopWord)
+                {
+                    isWork = false;
+                }
+            }
         }
+
+        private static List<string> SearchWords(string wordFound )
+        {
+            List<string> resultSearch = new List<string>( );
+            DirectoryInfo rootDir = Directory.CreateDirectory( Directory.GetCurrentDirectory( ) );
+            rootDir.Create( );
+
+            try
+            {
+                foreach (FileInfo cfile in rootDir.GetFiles( "*.xls", SearchOption.TopDirectoryOnly ))
+                {
+                    //Console.WriteLine( $"{cfile.FullName}" );
+                    ExtractExcelFiletoDateSet Excel = new ExtractExcelFiletoDateSet( 
+                        ConfigurationManager.ConnectionStrings[ "Excel16" ].ConnectionString, cfile.FullName);
+
+                    Excel.SearchWordInDataSet( wordFound );
+                }
+            }
+            catch (Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            return resultSearch;
+        }
+
+        
     }
 }
